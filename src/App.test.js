bfa7e31805c82349420837b2d5da0cc40e7f4370
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2020-05-17 01:31:56 -0600

    Basics of testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,14 @@
-test('Fake Test',() => {
- expect(true).toBeTruthy();//assertion
+import { add } from './App';
+
+//Unit Test: it only tests one thing
+test('add',() => {
+//     const value = add(1,2)
+//  expect(value).toBe(3);//assertion
+//refactoring
+expect(add(1 ,2)).toBe(3);
+expect(add(5 ,2)).toBe(7);
+//redundancy makes sure that there are no false positives.
 });
 
-test('Fake Two', () => {
-    expect(true).toBeTruthy();
-})
+
+
